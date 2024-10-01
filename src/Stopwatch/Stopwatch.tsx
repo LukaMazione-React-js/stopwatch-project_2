4ce4c452688bbee8b224c2e6fafe2501ac6b79cb
LukaMazione <luka.mazione@gmail.com>
2024-10-01 15:41:53 +0100

    Stopwatch and TimeDisplay - initial code

--- src/Stopwatch/Stopwatch.tsx
+++ src/Stopwatch/Stopwatch.tsx
@@ -1,24 +1,48 @@
 import { Button } from '../Button/Button';
 import stopwatchStyles from './stopwatch.module.scss';
 import buttonStyles from '../Button/button.module.scss';
+import { TimeDisplay } from '../TimeDisplay';
+import { useState, useEffect } from 'react';
 
 export const Stopwatch = () => {
+  const [totalTime, setTotalTime] = useState(0);
+  const [lapTime, setLapTime] = useState(0);
+
+  useEffect(() => {
+    const intervalTT = setInterval(() => {
+      setTotalTime(prev => prev + 10);
+    }, 10);
+
+    return () => clearInterval(intervalTT);
+  }, []);
+
+  useEffect(() => {
+    const intervalLT = setInterval(() => {
+      setLapTime(prev => prev + 10);
+    }, 10);
+
+    return () => clearInterval(intervalLT);
+  }, []);
+
   return (
-    <div className={stopwatchStyles.stopwatch}>
-      <div className={stopwatchStyles.buttonsLayout}>
-        <Button
-          className={buttonStyles.start}
-          text='START'
-        />
-        <Button
-          className={buttonStyles.stop}
-          text='STOP'
-        />
-        <Button
-          className={buttonStyles.lap}
-          text='LAP'
-        />
+    <>
+      <TimeDisplay totalTime={totalTime} lapTime={lapTime}/>
+      <div className={stopwatchStyles.stopwatch}>
+        <div className={stopwatchStyles.buttonsLayout}>
+          <Button
+            className={buttonStyles.start}
+            text='START'
+          />
+          <Button
+            className={buttonStyles.stop}
+            text='STOP'
+          />
+          <Button
+            className={buttonStyles.lap}
+            text='LAP'
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
---
+++ src/TimeDisplay.tsx
@@ -0,0 +1,19 @@
+type TimeDisplayProps = {
+  totalTime: number,
+  lapTime: number
+}
+export const TimeDisplay = ({totalTime, lapTime}: TimeDisplayProps) => {
+  
+  const formatTime = (timeInMS: number) => {
+    const minutes = Math.floor(timeInMS / (1000 * 60));
+    const seconds = Math.floor((timeInMS % (1000 * 60)) / 1000);
+    const miliseconds = Math.floor((timeInMS % 1000) / 10)
+  
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${miliseconds.toString().padStart(2, '0')}`;
+  };
+  
+  return <>
+  <h2>Total time: {formatTime(totalTime)}</h2>
+  <h2>Lap time: {formatTime(lapTime)}</h2>
+  </>;
+};
